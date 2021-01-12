658f165c3d86abfaeadc72ba567c36a137790e33
eric <ericlempe1994@gmail.com>
2021-01-12 17:20:46 -0300

    ajustes scripts datatable

--- config/adminlte.php
+++ config/adminlte.php
@@ -306,22 +306,27 @@ return [
 
     'plugins' => [
         'Datatables' => [
-            'active' => false,
+            'active' => true,
             'files' => [
                 [
                     'type' => 'js',
-                    'asset' => false,
-                    'location' => '//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js',
+                    'asset' => true,
+                    'location' => 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js',
                 ],
                 [
                     'type' => 'js',
-                    'asset' => false,
-                    'location' => '//cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js',
+                    'asset' => true,
+                    'location' => 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js',
+                ],
+                [
+                    'type' => 'js',
+                    'asset' => true,
+                    'location' => 'https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.23/af-2.3.5/b-1.6.5/b-colvis-1.6.5/b-html5-1.6.5/b-print-1.6.5/cr-1.5.3/fc-3.3.2/fh-3.1.7/kt-2.5.3/r-2.2.7/rg-1.1.2/rr-1.2.7/sc-2.0.3/sb-1.0.1/sp-1.2.2/sl-1.3.1/datatables.min.js',
                 ],
                 [
                     'type' => 'css',
-                    'asset' => false,
-                    'location' => '//cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css',
+                    'asset' => true,
+                    'location' => 'https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.23/af-2.3.5/b-1.6.5/b-colvis-1.6.5/b-html5-1.6.5/b-print-1.6.5/cr-1.5.3/fc-3.3.2/fh-3.1.7/kt-2.5.3/r-2.2.7/rg-1.1.2/rr-1.2.7/sc-2.0.3/sb-1.0.1/sp-1.2.2/sl-1.3.1/datatables.min.css',
                 ],
             ],
         ],
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,6 +37,17 @@
                             <th></th>
                         </tr>
                     </thead>
+                    <tbody>
+                        @foreach([1, 2, 3, 4, 5, 6] as $row)
+                        <tr>
+                            <td></td>
+                            <td>{{ $row }}</td>
+                            <td>Eric</td>
+                            <td>ericlempe1994@gmail.com</td>
+                            <td></td>
+                        </tr>
+                        @endforeach
+                    </tbody>
                 </table>
             </div>
         </div>
@@ -44,32 +55,23 @@
         
 @stop
 
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">   
-    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/adminlte/datatables/css/dataTables.bootstrap4.min.css') }}">  
-    <!-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.23/af-2.3.5/b-1.6.5/b-colvis-1.6.5/b-html5-1.6.5/b-print-1.6.5/cr-1.5.3/fc-3.3.2/fh-3.1.7/kt-2.5.3/r-2.2.7/rg-1.1.2/rr-1.2.7/sc-2.0.3/sb-1.0.1/sp-1.2.2/sl-1.3.1/datatables.min.css"/> -->
-@stop
-
 @section('js')
-	<!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
-	<script  -->type="text/javascript" src="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.23/af-2.3.5/b-1.6.5/b-colvis-1.6.5/b-html5-1.6.5/b-print-1.6.5/cr-1.5.3/fc-3.3.2/fh-3.1.7/kt-2.5.3/r-2.2.7/rg-1.1.2/rr-1.2.7/sc-2.0.3/sb-1.0.1/sp-1.2.2/sl-1.3.1/datatables.min.js"></script>
-
+	
     <script type="text/javascript">
         $(function () {
             
             var table = $('#users-table').DataTable({
                 processing: true,
-                serverSide: true,
-                ajax: "{{ route('admin.users.list') }}",
+                serverSide: false,
+                // ajax: "{{ route('admin.users.list') }}",
                 dom: 'Blfrtip',
                 buttons: [
-                    { extend: 'copyHtml5', text: '<i class="far fa-copy"></i>', titleAttr: 'Copiar' },
-                    { extend: 'excelHtml5', text: '<i class="far fa-file-excel"></i>', titleAttr: 'Excel' },
-                    { extend:'csvHtml5', text: '<i class="fas fa-file-csv"></i>', titleAttr: 'CSV' },
-                    { extend: 'pdfHtml5', text: '<i class="far fa-file-pdf"></i>', titleAttr: 'PDF' },
+                    { extend: 'copyHtml5', text: '<i class="far fa-copy"></i>', titleAttr: 'Copiar' },
+                    { extend: 'excelHtml5', text: '<i class="far fa-file-excel"></i>', titleAttr: 'Excel' },
+                    { extend:'csvHtml5', text: '<i class="fas fa-file-csv"></i>', titleAttr: 'CSV' },
+                    { extend: 'pdfHtml5', text: '<i class="far fa-file-pdf"></i>', titleAttr: 'PDF' },
                     { extend: 'print', text: '<i class="fas fa-print"></i>', titleAttr: 'Imprimir' }
-                ],
+                ],
                 columnDefs: [{
                     orderable: false,
                     className: 'select-checkbox',
@@ -79,22 +81,16 @@
                     style: 'multi',
                     selector: 'td:first-child'
                 },
-                columns: [
-                    {defaultContent: '', orderable: false, searchable: false },
-                    {data: 'id', name: 'id'},
-                    {data: 'name', name: 'name'},
-                    {data: 'email', name: 'email'},
-                    {data: 'action', name: 'action', orderable: false, searchable: false},
-                ],
+                // columns: [
+                //     {defaultContent: '', orderable: false, searchable: false },
+                //     {data: 'id', name: 'id'},
+                //     {data: 'name', name: 'name'},
+                //     {data: 'email', name: 'email'},
+                //     {data: 'action', name: 'action', orderable: false, searchable: false},
+                // ],
                 initComplete: function () {
-                    // table.buttons().container().appendTo( '#users-table_wrapper .col-md-6:eq(0)' );
                 }   
-            }).buttons().container().appendTo('#users-table_wrapper .col-md-6:eq(0)');
-
-       
-
-            
-            
-    });
+            });
+        });
 </script>
 @stop
